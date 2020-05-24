6450f97f78e6cb1bf891a707b16f0f746fd16e34
Ambuj Rajput <arajput@ingenuitygaming.com>
2020-05-24 20:26:11 +0530

    calculator app

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, resp) => {
-    resp.sendFile(__dirname + "index.html");
+    resp.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, resp) => {
+    console.log(req.body);
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+
+    var result = num1 + num2;
+
+    resp.send("The result of calculation is " + result);
 });
 
 app.listen(3000, () => {
